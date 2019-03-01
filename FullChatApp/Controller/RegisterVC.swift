d01de2ae52ecd06fa82a7a946885c02f7f0e6191
Akil Kumar <akil.kumar.thota@autodesk.com>
2019-03-01 14:36:36 -0500

    registervc UI and setup completed

--- FullChatApp/Controller/RegisterVC.swift
+++ FullChatApp/Controller/RegisterVC.swift
@@ -1,5 +1,5 @@
 //
-//  LoginVC.swift
+//  RegisterVC.swift
 //  FullChatApp
 //
 //  Created by Akil Kumar Thota on 3/1/19.
@@ -8,28 +8,33 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class RegisterVC: UIViewController {
+    
     
     //MARK:- IBOutlets
-    
     
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
+    
     //MARK:- Properties
     
     
-    //MARK:- Life Cycle Methods
+    
+    //MARK:- Life cycle methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
     
     
     //MARK:- Functions
-    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
@@ -39,17 +44,8 @@ class LoginVC: UIViewController {
     
     //MARK:- IBActions
     
-    
-    @IBAction func loginButtonTapped(_ sender: Any) {
-        guard let email = emailTextField.text, email != "" else {
-            return
-        }
-        guard let password = passwordTextField.text, password != "" else {
-            return
-        }
-        print(email,password)
+    @IBAction func signUpButtonTapped(_ sender: Any) {
     }
-
     
-}
 
+}
